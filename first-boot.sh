82665c04433ceb9ea7c0e172d9b149ae6e484060
Sid Roberts <sid@sidroberts.co.uk>
2022-06-29 07:35:04 +0900

    Fixed VS Code config paths.

--- first-boot.sh
+++ first-boot.sh
@@ -523,11 +523,11 @@ fi
 
 sudo pacman -S code --noconfirm --needed
 
-mkdir -p "~/.config/Code - OSS/"
-mkdir -p "~/.config/Code - OSS/User/"
+mkdir -p ~/.config/"Code - OSS"/
+mkdir -p ~/.config/"Code - OSS"/User/
 
-cp vs-code/Preferences   "~/.config/Code - OSS/Preferences"
-cp vs-code/settings.json "~/.config/Code - OSS/User/settings.json"
+cp vs-code/Preferences   ~/.config/"Code - OSS"/Preferences
+cp vs-code/settings.json ~/.config/"Code - OSS"/User/settings.json
 
 
 
