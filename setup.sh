63ed38fbd35767310afb9bc5ad34191e73932a7a
Sid Roberts <sid@sidroberts.co.uk>
2023-11-18 23:01:07 +0900

    Moved VS Code repo config to its own file.

--- setup.sh
+++ setup.sh
@@ -487,7 +487,7 @@ fi
 ###########
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo cp ${SCRIPT_DIR}/vs-code/vscode.repo /etc/yum.repos.d/vscode.repo
 
 sudo dnf -y install code
 
